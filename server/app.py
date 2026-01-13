26836859040cf7276b1a7f5875400548387fa964
Your Name <youremail@example.com>
2026-01-13 20:05:57 +0300

    finished assignment

--- server/app.py
+++ server/app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_migrate import Migrate
+import os
 
 from models import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/jonah/python-p4-v2-flask-sqlalchemy-lab-2/server/instance/app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 migrate = Migrate(app, db)
